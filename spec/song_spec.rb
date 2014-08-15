b5b1e675182e03eaa2259f8e24f58da0396697c1
Linda Gibson <dakidz@live.com>
2014-08-15 16:30:55 -0700

    initializes a new song returnig the name

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,1 +1,14 @@
+require 'rspec'
+require 'song'
 
+describe 'Song' do
+  it 'initializes with a new song' do
+    test_song = Song.new({:name => 'Love Story'})
+    expect(test_song).to be_an_instance_of Song
+  end
+
+  it 'returns the name of the song' do
+    test_song = Song.new({:name => 'Love Story'})
+    expect(test_song.name).to eq 'Love Story'
+  end
+end
