aac7ce6d455d234de098af038b09bdcb13b56355
Linda Gibson <dakidz@live.com>
2014-08-15 18:21:52 -0700

    add main menu

--- playlist.rb
+++ playlist.rb
@@ -1,1 +1,38 @@
+require './lib/artist'
+require './lib/song'
 
+def main_menu
+  system('clear')
+  loop do
+    choice = nil
+    until choice == 'e'
+      puts "Press the corresponding letter to select choice."
+      puts "[a] Add an artist"
+      puts "[n] Add a new song"
+      puts "[l] List all artists"
+      puts "[v] View all songs"
+      puts "[s] Search for an artist and list songs"
+      puts "[x] exit"
+
+      choice = gets.chomp
+      case choice
+      when 'a'
+        add_artist
+      when 'n'
+        add_song
+      when 'l'
+        list_artists
+      when 'v'
+        list_songs
+      when 's'
+        Search
+      when 'x'
+        puts "Good-bye!"
+        exit
+      else
+        puts "Sorry, that wasn't a valid option."
+      end
+    end
+  end
+end
+main_menu
